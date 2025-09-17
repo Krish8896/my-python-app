a2c1a611094abec4028fbfef2489ab938edb064c
Krish8896 <krishna77kicha@gmail.com>
2025-09-17 13:14:26 +0530

    updated app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+# app.py
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+# MySQL configuration
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="yourpassword",
+    database="yourdb"
+)
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT id, name, email FROM users;")
+    users = cursor.fetchall()
+    return jsonify(users)
+
+@app.route('/api/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    cursor = db.cursor()
+    cursor.execute(
+        "INSERT INTO users (name, email) VALUES (%s, %s)",
+        (data['name'], data['email'],)
+    )
+    db.commit()
+    return jsonify({"status": "User added"}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
