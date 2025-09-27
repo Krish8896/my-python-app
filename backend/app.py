b8bb13d60f150ed12b9db3557c1c8a3b805eb1ee
Krish8896 <krishna77kicha@gmail.com>
2025-09-27 21:58:22 +0530

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -35,4 +35,4 @@ def add_user():
     return jsonify({"status": "User added"}), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0/0', port=5000, debug=True)
