cbf630bc4588b9e780fd4c28972cdb1568e31f83
Krish8896 <krishna77kicha@gmail.com>
2025-10-04 16:07:31 +0530

    updated backend/test_app.py

--- backend/test_app.py
+++ backend/test_app.py
@@ -5,3 +5,10 @@ class BasicTests(unittest.TestCase):
     def setUp(self):
         self.app = create_app().test_client()
         self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/api/users')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
