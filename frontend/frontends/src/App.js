098e6b4db1510d8485188c175af8641804315470
Krish8896 <krishna77kicha@gmail.com>
2025-10-25 16:02:28 +0530

    updated Jenkinsfile frontend/frontends/src/App.js

--- frontend/frontends/src/App.js
+++ frontend/frontends/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const API_URL = "http://13.53.170.118:5000/api/users"; // your Flask backend
+  const API_URL = "http://16.171.47.48:5000/api/users"; // your Flask backend
 
   // Fetch all users
   const fetchUsers = async () => {
